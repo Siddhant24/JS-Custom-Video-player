441c6b06ea3847142a158d866061e921db8bde3b
Siddhant24 <sidbhakar@gmail.com>
2017-06-05 01:59:36 +0530

    added mouseout event handler

--- scripts.js
+++ scripts.js
@@ -57,3 +57,4 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+progress.addEventListener('mouseout', () => mousedown = false);
